47790adc38732e876374d423a64e4897971d2df3
gcooney29 <gcooney@colgate.edu>
2019-12-13 20:32:23 -0500

    cleaned up script, poster plots

--- plots.R
+++ plots.R
@@ -18,8 +18,8 @@ percen$Dia_Earth <- percen$Dia_Earth *100
 #create plot with legend
 par(mar=c(5.1, 4.2, 4.2, 8.1), xpd=TRUE)
 plot(percen$Day, percen$Baking_Soda, type = "l", lty = 1, lwd = 1.5, 
-     xlab = "Day of Study", ylab = "Percent Dead",
-     cex.axis = .8, cex.lab = .9, font.lab = 2)
+     xlab = "Day of study", ylab = "Death (percentage)",
+     cex.axis = .8, cex.lab = .9, font.lab = 1)
 lines(percen$Day, percen$Control, type = "l", lty = 1, col = "grey73", lwd = 2.5)
 lines(percen$Day, percen$Dia_Earth, type = "l", lty = 3, lwd = 1.5)
 lines(percen$Day, percen$Vinegar, type = "l", lty = 4, lwd = 1.5)
@@ -57,20 +57,30 @@ ggplot(wc_sum) +
   geom_bar(aes(x=name, y=mean), stat="identity", alpha=0.7, width = .5) +
   geom_errorbar( aes(x=name, ymin=mean-sd, ymax=mean+sd), width=0.2, 
                  colour="black", alpha=0.9, size=.8) +
-  xlab("Treatment Type") + ylab("Weight Conserved (percentage)") +
+  xlab("Treatment") + ylab("Weight conserved (percentage)") +
+  scale_x_discrete(breaks=1:3,
+                     labels=c("Biochar","Control","Diatomaceous Earth")) +
   ylim(000, 115) + theme_bw() +
-  theme(axis.title.x = element_text(margin = margin(l = 0, r = 0, t = 20, b = 0),
-                                    face = "bold")) + 
-  theme(axis.title.y = element_text(margin = margin(l = 0, r = 20, t = 0, b = 0),
-                                    face = "bold")) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank())
+  theme(axis.title.x = element_text(margin = margin(l = 0, r = 0, t = 20, b = 0))) + 
+  theme(axis.title.y = element_text(margin = margin(l = 0, r = 20, t = 0, b = 0))) +
   theme(axis.ticks.length = unit(.2, "cm"), 
         axis.text.x = element_text(vjust = -3), 
         axis.text.y = element_text(hjust = -.1), 
-        panel.border = element_rect(colour = "black", fill=NA, size=.7)) 
+        panel.border = element_rect(colour = "black", fill=NA, size=.7))
+   
+#ANOVA? 
+#levene test
+leveneTest(days_survived ~ Treatment, data = DS) #no homogeneity
 
-
-#ANOVA
+#Kruskall wallis test
 DS <- read.csv("days_survived.csv")
+
+kruskal.test(days_survived ~ Treatment, data = DS)
+
+#Dunn Test
+dunn.test::dunn.test(DS$days_survived, DS$Treatment) 
 
 #group data (mean, sd)
 group_by(DS, Treatment) %>%
@@ -79,27 +89,12 @@ group_by(DS, Treatment) %>%
     mean = mean(days_survived, na.rm = TRUE),
     sd = sd(days_survived, na.rm = TRUE)
   )
-
-#Compute the analysis of variance
-DS.aov <- aov(days_survived ~ Treatment, data = DS)
-#Summary of the analysis
-summary(DS.aov)
-
-#Tukey test
-TukeyHSD(DS.aov)
-
-#boxplot with Tukey results
-DS$posthoc[DS$Treatment == "Baking_Soda"] <- "a"
-DS$posthoc[DS$Treatment == "Biochar"] <- "b"
-DS$posthoc[DS$Treatment == "Control"] <- "b"
-DS$posthoc[DS$Treatment == "Dia_Earth"] <- "b"
-DS$posthoc[DS$Treatment == "Vinegar"] <- "c"
-
-boxplot(DS$days_survived ~ DS$Treatment, ylab = "Days Survived",
-        xlab = "Treatment", names = c("Baking Soda", "Biochar",
+#boxplot of results
+boxplot(DS$days_survived ~ DS$Treatment, ylab = "Days survived",
+        xlab = "Treatment", names = c("Baking soda", "Biochar",
                                       "Control", " ",
                                       "Vinegar"), font.lab = 2)
-mtext(text= "Diatomaceous\nEarth",
+mtext(text= "Diatomaceous\nearth",
       side = 1, line = c(2), at = c(4))
 text(x= 1, y= 10, labels= "a")
 text(x= 2, y= 15, labels= "b")
@@ -107,13 +102,34 @@ text(x= 3, y= 15, labels= "b")
 text(x= 4, y= 15, labels= "b")
 text(x= 5, y= 1, labels= "c")
 
-#levene test
-leveneTest(days_survived ~ Treatment, data = DS) #no homogeneity
+#create plot with legend (for poster!)
+par(mar=c(5.1, 4.2, 4.2, 8.1), xpd=TRUE)
+plot(percen$Day, percen$Vinegar, type = "l", lty = 1, col = "blue", lwd = 1.5, 
+     xlab = "Day of Study", ylab = "Death (percentage)",
+     cex.axis = .8, cex.lab = .9, font.lab = 2)
+lines(percen$Day, percen$Control, type = "l", lty = 1, col = "black", lwd = 1.5)
+lines(percen$Day, percen$Dia_Earth, type = "l", lty = 1, col = "goldenrod1", lwd = 1.5)
+lines(percen$Day, percen$Baking_Soda, type = "l", lty = 1, col = "red3", lwd = 1.5)
+lines(percen$Day, percen$Biochar, type = "l", lty = 1, col = "lightskyblue", lwd = 1.5)
+legend("topright", inset=c(-.37, -.087), 
+       legend = c("\n Control \n", "\n Vinegar \n", "\n Baking soda \n", "\n Biochar \n", "\n Diatomaceous \n earth"), 
+       title="Treatment Type", col = c("black", "blue", "red3", "lightskyblue", "goldenrod1"),
+       lty = c(1, 1, 1, 1, 1), lwd = c(1.5, 1.5, 1.5, 1.5, 1.5), cex = 0.69, 
+       ncol = 1, text.width = 3.5, box.lty = 0)
 
-#replace ANOVA with Kruskal-Wallis test
-kruskal.test(days_survived ~ Treatment, data = DS)
-
-#replace Tukey with Dunn Test
-dunn.test::dunn.test(DS$days_survived, DS$Treatment) 
-#boxplot stays the same, all significant results are the same
+#boxplot for poster
+boxplot(DS$days_survived ~ DS$Treatment, ylab = expression(bold("Days Survived")),
+        xlab = expression(bold("Treatment")), names = c("Baking Soda", "Biochar",
+                                      "Control", " ",
+                                      "Vinegar"), col = c("red3", "cornflowerblue",
+                                                          "white", "goldenrod1", "blue"),
+        font.lab = 2)
+mtext(text= "Diatomaceous\nEarth",
+      side = 1, line = c(2), at = c(4))
+text(x= 1, y= 10, labels= "a")
+text(x= 2, y= 15, labels= "b")
+text(x= 3, y= 15, labels= "b")
+text(x= 4, y= 15, labels= "b")
+text(x= 5, y= 1, labels= "c")
+font("xlab", face = "bold")
 
